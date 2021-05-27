f438f63494c4e5eb298ae81b06b9d11d3fc626b7
Suryam26 <suryam.jain@gmail.com>
2021-05-27 22:39:15 +0530

    Create, update and delete views added

--- questions/urls.py
+++ questions/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from .views import HomeView, PaperDetailView, PaperCreateView
+from .views import HomeView, PaperDetailView, PaperCreateView, MyListView, PaperUpdateView, PaperDeleteView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
-    path('<int:pk>/detail/', PaperDetailView.as_view(), name='detail'),
-    path('create/', PaperCreateView.as_view(), name='create')
+    path('paper/<int:pk>/detail/', PaperDetailView.as_view(), name='detail'),
+    path('paper/new/', PaperCreateView.as_view(), name='create'),
+    path('paper/mylist/', MyListView.as_view(), name='mylist'),
+    path('paper/<int:pk>/update', PaperUpdateView.as_view(), name='update'),
+    path('paper/<int:pk>/delete', PaperDeleteView.as_view(), name='delete'),
 ]
--- questions/views.py
+++ questions/views.py
@@ -1,6 +1,6 @@
-from re import L
-from django.views.generic import ListView, DetailView, CreateView
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.urls import reverse_lazy
 
 from .models import QuestionPaper
 
@@ -11,13 +11,47 @@ class HomeView(LoginRequiredMixin, ListView):
     context_object_name = 'papers'
 
 
-class PaperDetailView(DetailView):
+class PaperDetailView(LoginRequiredMixin, DetailView):
     model = QuestionPaper
     template_name = 'questions/detail.html'
     context_object_name = 'paper'
 
 
-class PaperCreateView(CreateView):
+class PaperCreateView(LoginRequiredMixin, CreateView):
     model = QuestionPaper
-    fields = '__all__'
+    fields = ['branch', 'subject', 'exam', 'year', 'semester', 'paper']
     template_name = 'questions/create.html'
+    success_url = reverse_lazy('mylist')
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class MyListView(LoginRequiredMixin, ListView):
+    model = QuestionPaper
+    template_name = 'questions/mylist.html'
+    context_object_name = 'mylist'
+
+    def get_queryset(self):
+        return self.model.objects.filter(author=self.request.user).order_by('-updated_at')
+
+
+class PaperUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = QuestionPaper
+    fields = ['branch', 'subject', 'exam', 'year', 'semester', 'paper']
+    template_name = 'questions/update.html'
+    success_url = reverse_lazy('mylist')
+
+    def test_func(self):
+        return self.get_object().author == self.request.user
+
+
+class PaperDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = QuestionPaper
+    template_name = 'questions/delete.html'
+    context_object_name = 'paper'
+    success_url = reverse_lazy('mylist')
+
+    def test_func(self):
+        return self.get_object().author == self.request.user
